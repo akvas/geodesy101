6475e884a2679f66f001dcd809c0578417b75981
Andreas Kvas <kvas@tugraz.at>
2022-05-24 17:40:12 +0200

    added comments and tasks

--- example01.py
+++ example01.py
@@ -5,6 +5,7 @@ import cartopy as ctp
 import urllib.request
 
 
+# Download a GRACE L2 Solution from TU Graz FTP server
 ftp_url = 'ftp.tugraz.at'
 data_directory = 'outgoing/ITSG/GRACE/ITSG-Grace2018/monthly/monthly_n96'
 file_name = 'ITSG-Grace2018_n96_2008-05.gfc'
@@ -15,27 +16,33 @@ with FTP(ftp_url) as ftp:
     with open(f'{file_name}', 'wb') as fp:
         ftp.retrbinary(f'RETR {file_name}', fp.write)
 
+# import the downloaded file and a static field as PotentialCoefficients instance
 monthly_l2_field = gt.io.loadgfc('ITSG-Grace2018_n96_2008-05.gfc')
 static_field = gt.io.loadgfc('data/GOCO06s_aodReduced_static_2010-01-01.gfc')
 
-monthly_l2_field -= static_field
-monthly_l2_field.truncate(96)
-shc_filter = gt.filter.Gaussian(350)
+monthly_l2_field -= static_field  # reduce the static field to reveal the time-variable signal 
+monthly_l2_field.truncate(96)  # truncate the reduced field to the original maximum spherical harmonic degree
 
-monthly_l2_filtered = shc_filter.filter(monthly_l2_field)
+shc_filter = gt.filter.Gaussian(350)  # Create a Gaussian filter object with 300 km filter radius
+monthly_l2_filtered = shc_filter.filter(monthly_l2_field)  # apply the filter to the L2 solution 
 
-mass_change_grid = monthly_l2_filtered.to_grid(kernel='ewh')
+mass_change_grid = monthly_l2_filtered.to_grid(kernel='ewh')  # propagate the potential coefficients to gridded equivalent water height
 
+# visualize the grid with cartopy
 f, ax = plt.subplots(subplot_kw={'projection': ctp.crs.Robinson()})
 im = ax.imshow(mass_change_grid.value_array * 100, cmap='RdBu', vmin=-25, vmax=25, transform=ctp.crs.PlateCarree())
 ax.coastlines()
 gt.plot.colorbar(im, ax, label='water storage [cm]', extend='both')
 plt.savefig('l2_solution.png', bbox_inches='tight', transparent=True, dpi=300)
 
+# Download the GSFC GRACE/GRACE-FO mascons from their website
 urllib.request.urlretrieve('https://earth.gsfc.nasa.gov/sites/default/files/2022-05/gsfc.glb_.200204_202112_rl06v2.0_obp-ice6gd.h5', 'gsfc.glb_.200204_202112_rl06v2.0_obp-ice6gd.h5')
+
+# create a mascon TimeSeries object from the data file
 gfsc_time_series = gt.io.loadgsfc06mascons('gsfc.glb_.200204_202112_rl06v2.0_obp-ice6gd.h5')
 gsfc_grid = gfsc_time_series[0]
 
+# visualize the mascon solution as colored surface tiles
 f, ax = plt.subplots(subplot_kw={'projection': ctp.crs.Robinson()})
 ax.set_global()
 gsfc_grid.values *= 100
--- example02.py
+++ example02.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from ftplib import FTP
 import cartopy as ctp
 
-
+# Download a GRACE L2 Solution from TU Graz FTP server
 ftp_url = 'ftp.tugraz.at'
 data_directory = 'outgoing/ITSG/GRACE/ITSG-Grace2018/monthly/monthly_n96'
 file_name = 'ITSG-Grace2018_n96_2008-05.gfc'
@@ -14,23 +14,32 @@ with FTP(ftp_url) as ftp:
     with open(f'{file_name}', 'wb') as fp:
         ftp.retrbinary(f'RETR {file_name}', fp.write)
 
+# import the downloaded file and a static field as PotentialCoefficients instance
 monthly_l2_field = gt.io.loadgfc('ITSG-Grace2018_n96_2008-05.gfc')
 static_field = gt.io.loadgfc('data/GOCO06s_aodReduced_static_2010-01-01.gfc')
 
-monthly_l2_field -= static_field
-monthly_l2_field.truncate(96)
+monthly_l2_field -= static_field  # reduce the static field to reveal the time-variable signal 
+monthly_l2_field.truncate(96)  # truncate the reduced field to the original maximum spherical harmonic degree
 
-mass_change_grid = monthly_l2_field.to_grid(kernel='ewh')
+mass_change_grid = monthly_l2_field.to_grid(kernel='ewh')  # propagate the unfiltered solution to equivalent water height
 
+# visualize the unfiltered grid with cartopy
 f, ax = plt.subplots(subplot_kw={'projection': ctp.crs.Robinson()})
 im = ax.imshow(mass_change_grid.value_array * 100, cmap='RdBu', vmin=-25, vmax=25, transform=ctp.crs.PlateCarree())
 ax.coastlines()
 gt.plot.colorbar(im, ax, label='water storage [cm]', extend='both')
 plt.savefig('l2_solution_unfiltered.png', bbox_inches='tight', transparent=True, dpi=300)
 
-
+# initialize the filter dictionary we want to apply
+# - Gaussian filter are simple isotropic and homogenous filters and are defined by their 
+#   filter radius (300 km in this example). The large the filter radius, the stronger the filter. 
+# - DDK filters are anistropic and designed specifically to reduce GRACE/GRACE-FO noise. They
+#   are defined by their level, which typically ranges from 1-8
+# 
+# Try different values for filter radius and DDK level!
 shc_filters = {'Gauss_300km': gt.filter.Gaussian(300), 'DDK3': gt.filter.DDK(3)}
 
+# visualize the filetered grid with cartopy
 for filter_name, shc_filter in shc_filters.items():
     filtered_coefficients = shc_filter.filter(monthly_l2_field)
 
